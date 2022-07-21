097a58a0ed0e4f2cb7e3f40cef89edf8589d0a8b
Numan Naeem <numan.naeem9@gmail.com>
2022-07-21 16:14:54 +0530

    fix window problem

--- src/components/layout.js
+++ src/components/layout.js
@@ -11,29 +11,39 @@ import Navbar from './Navbar'
 
 export const CursorContext = React.createContext('dog')
 
-const Layout = ({ children }) => {
+const isBrowser = typeof window !== 'undefined'
 
+const Layout = ({ children }) => {
+  
   const [cursor, setCursor] = React.useState(
-    window?.localStorage?.getItem('cursor') || 'default'
+    isBrowser ? window.localStorage.getItem('cursor') || 'default' : 'default'
   )
 
   return (
-    <CursorContext.Provider value={{ cursor, setCursor: (option) => {
-      localStorage.setItem('cursor', option)
-      setCursor(option)
-    } }}>
+    <CursorContext.Provider
+      value={{
+        cursor,
+        setCursor: option => {
+          localStorage.setItem('cursor', option)
+          setCursor(option)
+        }
+      }}
+    >
       <div
-        style={ cursor !== 'default' ? {
-          cursor: `url(svgs/${cursor}.svg) 16 16, auto`
-        } : {}}
+        style={
+          cursor !== 'default'
+            ? {
+                cursor: `url(svgs/${cursor}.svg) 16 16, auto`
+              }
+            : {}
+        }
         className='bg-gray-900 text-white flex flex-col min-h-screen'
       >
         <Navbar />
-        <div className='flex flex-col content-center grow'>
-          {children}
-        </div>
+        <div className='flex flex-col content-center grow'>{children}</div>
         <footer className='pt-[3rem] text-gray-300 text-center  my-8'>
-          © {new Date(Date.now()).getFullYear()} • because every website needs a footer
+          © {new Date(Date.now()).getFullYear()} • because every website needs a
+          footer
         </footer>
       </div>
     </CursorContext.Provider>
